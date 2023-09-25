70efd324fc03eca4ffa133c642a9f6942763e929
dfredriksson-sdsu <146035954+dfredriksson-sdsu@users.noreply.github.com>
2023-09-25 15:04:24 -0700

    Delete testing.m

--- testing.m
+++
@@ -1,7 +0,0 @@
-function testing
-%Function that can be used for testing the NCA GUI Button callbacks
-%Will give positive reinforcement to the user if his/her code runs..LOL
-
-disp('Your code runs, you genius!');
-
-end
